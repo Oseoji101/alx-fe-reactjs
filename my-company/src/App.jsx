c1dff8080fe7f99dc34810ce6a4158fd753793ce
oseoji101 <nonso.kelvin8770@gmail.com>
2025-09-05 19:52:35 +0100

    .

---
+++ my-company/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Services from './components/Services';
+
+function App() {
+    return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App;
