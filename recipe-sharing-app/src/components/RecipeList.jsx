3a070bd463cdb8b6e69ae52f11505c781bfc3bf8
oseoji101 <nonso.kelvin8770@gmail.com>
2025-09-12 15:24:34 +0100

    Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import useRecipeStore from './recipeStore.js';
+import { Link } from 'react-router-dom';
 
   const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
@@ -7,7 +8,9 @@ import useRecipeStore from './recipeStore.js';
       <div>
         {recipes.map(recipe => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+            <h3>
+              <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+            </h3>
             <p>{recipe.description}</p>
           </div>
         ))}
