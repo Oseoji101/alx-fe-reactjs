6d1b3c968de3f06c24206dcff2e7a2ceb7446168
oseoji101 <nonso.kelvin8770@gmail.com>
2025-10-08 17:40:36 +0100

    initial commit

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+const RegistrationForm = () => {
+const [formData, setFormData] = useState({username: "", email: "", password: ""});
+
+const handleChange = (e) => {
+    const {name, value} = e.target
+    setFormData((prevData) => ({formData, [name]: value}))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+    if(!formData.username || !formData.email || !formData.password){
+        alert("All fields are required");
+        
+        return;
+    }
+    console.log("Form submitted", formData);
+
+    return (
+    <form onSubmit={handleSubmit}>
+        <h2>Registration form</h2>
+
+        <label>
+            username:
+            <input type="text" 
+            name="username"
+            value={formData.username}
+            placeholder="Enter your username"
+            onChange={handleChange}/>
+
+        </label>
+
+        <label>
+            email:
+            <input type="email" 
+            name="email"
+            value={formData.email}
+            placeholder="Enter your email"
+            onChange={handleChange}/>
+        </label>
+        
+        <label>
+            <input type="password" 
+            name="password"
+            value={formData.password}
+            placeholder="Enter your password"
+            onChange={handleChange}/>
+        </label>
+        <button type="submit">Register</button>
+    </form>
+);
+}
+export default RegistrationForm;
---
+++ form-handling-react/src/components/formikForm.js
@@ -0,0 +1,80 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+
+const validation = Yup.object({
+  username: Yup.string()
+    .min(3, "Username must be at least 3 characters")
+    .required("Username is required"),
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
+
+function FormikForm() {
+  return (
+    <div>
+      <h2>User Registration (Formik)</h2>
+
+      
+      <Formik
+        initialValues={{
+          username: "",
+          email: "",
+          password: "",
+        }}
+        validation={validation}
+        onSubmit={(values, { resetForm }) => {
+          console.log("Submitted:", values);
+          alert("Form submitted successfully!");
+          resetForm(); 
+        }}
+      >
+        
+        <Form >
+          <label>
+            Username:
+            
+            <Field name="username" placeholder="Enter username" />
+            <ErrorMessage
+              name="username"
+              component="div"
+              style={{ color: "red", fontSize: "0.8em" }}
+            />
+          </label>
+
+          <label>
+            Email:
+            <Field name="email" placeholder="Enter email" />
+            <ErrorMessage
+              name="email"
+              component="div"
+              style={{ color: "red", fontSize: "0.8em" }}
+            />
+          </label>
+
+          <label>
+            Password:
+            <Field
+              name="password"
+              type="password"
+              placeholder="Enter password"
+            />
+            <ErrorMessage
+              name="password"
+              component="div"
+              style={{ color: "red", fontSize: "0.8em" }}
+            />
+          </label>
+
+          <button type="submit">Register</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default FormikForm;
