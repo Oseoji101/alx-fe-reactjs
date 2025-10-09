f17ac01ca56cbce6b5b6f30755175abbc342498f
oseoji101 <nonso.kelvin8770@gmail.com>
2025-10-09 15:25:49 +0100

    update Registration form

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -3,12 +3,8 @@ import * as Yup from "yup";
 
 
 const validationSchema = Yup.object({
-  username: Yup.string()
-    .min(3, "Username must be at least 3 characters")
-    .required("Username is required"),
-  email: Yup.string()
-    .email("Invalid email format")
-    .required("Email is required"),
+  username: Yup.string().required("Username is required"),
+  email: Yup.string().required("Email is required"),
   password: Yup.string()
     .min(6, "Password must be at least 6 characters")
     .required("Password is required"),
